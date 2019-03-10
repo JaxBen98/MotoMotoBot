fe7b1be4bbcb5d22842b97c33cfd175773014d86
PandaBoi <buddabane5@gmail.com>
2019-03-10 13:29:21 -0400

    moto moto is back in action

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    client.user.setGame('with hot ladies');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === '!ping') {
+
+       message.reply('!!!ping received!!!');
+
+       }
+
+});
+
+client.login(process.env.BOT_TOKEN);
